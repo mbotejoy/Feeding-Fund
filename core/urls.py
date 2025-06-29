c1d9c4aff0f6fe1280b350871d610aa7c622f7bb
mbotejoy <joy.mbote@strathmore.edu>
2025-06-29 22:32:20 +0300

    Connected all the dashboards to the home page

--- core/urls.py
+++ core/urls.py
@@ -30,15 +30,28 @@ urlpatterns = [
     #This is for the homepage
     path('', views.homepage, name='homepage'),
 
+    #This is for the About Us page
+    path('', views.about_us, name='about_us'),
+
+    #This is for the Impact page
+    path('', views.impact, name='impact'),
+
+    # Link for the signup page
+    path('signup/', views.signup, name='signup'),
+
+    # Link for the School Admin Dashboard 
+    path('school_admin-dashboard/', views.school_admin, name='school_admin'),
+
     #Community Agent dashboard view
     path('communityagent-dashboard/', views.communityagent_dashboard, name='communityagent_dashboard'),
+
+    #Parent dashboard view
+    path('parent-dashboard/', views.parent, name='parent'),
 
     path('register-school/', views.register_school, name='register_school'),
     
     path('submit_feeding_report/', views.submit_feeding_report, name='submit_feeding_report'),
 
-    # Link for the signup page
-    path('signup/', views.signup, name='signup'),
 
 ]
 
