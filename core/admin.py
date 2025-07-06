be0522f79de1d641b61cf64346d5962380cbd356
mbotejoy <joy.mbote@strathmore.edu>
2025-07-06 14:08:14 +0300

    Removed select dropdown and made restricetd access to users

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
-from .models import Role
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Role
 
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('email', 'full_name', 'role', 'is_verified', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'role', 'is_verified')
+    search_fields = ('email', 'full_name')
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('full_name', 'role')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_verified', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'full_name', 'password1', 'password2', 'role', 'is_verified', 'is_staff', 'is_active')}
+        ),
+    )
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Role)
